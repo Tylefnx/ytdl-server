aec56c2b909920cb0b68f72178e0aa5f2c26168d
tylefnx <tylefnx@basedlaptop.tailc002fd.ts.net>
2026-01-02 07:12:24 +0300

    Created a new route

--- internal/api/router.go
+++ internal/api/router.go
@@ -4,14 +4,13 @@ import (
 	"net/http"
 )
 
-// NewRouter setup routes and apply global middleware
 func NewRouter(h *Handler) http.Handler {
-	mux := http.NewServeMux()
+    mux := http.NewServeMux()
 
-	mux.HandleFunc("/api/job", h.CreateJob)
-	mux.HandleFunc("/api/events/", h.SSE)
-	mux.HandleFunc("/api/download/", h.Download)
+    mux.HandleFunc("/api/job", h.CreateJob)
+    mux.HandleFunc("/api/info", h.GetVideoInfo) // <-- h.CreateJob yerine h.GetVideoInfo
+    mux.HandleFunc("/api/events/", h.SSE)
+    mux.HandleFunc("/api/download/", h.Download)
 
-	// Wrap everything with our robust CORS logic
-	return CORSMiddleware(mux)
+    return CORSMiddleware(mux)
 }
